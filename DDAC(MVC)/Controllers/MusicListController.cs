ef7ae1a3452504afbf8be4552e33a92e3e4d391d
ChuahSengYew <sychuah42@gmail.com>
2022-01-19 21:49:46 +0800

    Add project files.

---
+++ DDAC(MVC)/Controllers/MusicListController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDAC_MVC_.Controllers
+{
+    public class MusicListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
